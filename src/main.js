ab5f9b337f3280a4a5f1673dd60119533f620ad6
Leonardo Silveira <sombriks@gmail.com>
2020-11-02 14:22:50 -0300

    controls

--- src/main.js
+++ src/main.js
@@ -1,35 +1,56 @@
 // rollup is the new browserify
-import "./main.scss"
-import stage1 from "./maps/stage1"
+import "./main.scss";
+import stage1 from "./maps/stage1";
 
-const mapa = stage1()
+const mapa = stage1();
 
 const player = {
-  c: '@', y: 0, x: 0,
-  up() { if (this.y > 0) this.y--; paint() },
-  down() { if (this.y < mapa.length-1) this.y++; paint() },
-  left() { if (this.x > 0) this.x--; paint() },
-  right() { if (this.x < mapa[0].length-1) this.x++; paint() }
-}
+  c: "@",
+  y: 0,
+  x: 0,
+  up() {
+    if (this.y > 0) this.y--;
+    paint();
+  },
+  down() {
+    if (this.y < mapa.length - 1) this.y++;
+    paint();
+  },
+  left() {
+    if (this.x > 0) this.x--;
+    paint();
+  },
+  right() {
+    if (this.x < mapa[0].length - 1) this.x++;
+    paint();
+  },
+};
 
-document.addEventListener("keydown", e => {
+document.addEventListener("keydown", (e) => {
   switch (e.key) {
-    case 'ArrowUp': return player.up()
-    case 'ArrowDown': return player.down()
-    case 'ArrowLeft': return player.left()
-    case 'ArrowRight': return player.right()
+    case "ArrowUp":
+      return player.up();
+    case "ArrowDown":
+      return player.down();
+    case "ArrowLeft":
+      return player.left();
+    case "ArrowRight":
+      return player.right();
   }
-})
+});
 
-document.addEventListener("click", e => {
-  console.log(e)
-})
+document.getElementById("up").addEventListener("click", (e) => player.up());
+document.getElementById("down").addEventListener("click", (e) => player.down());
+document.getElementById("left").addEventListener("click", (e) => player.left());
+document
+  .getElementById("right")
+  .addEventListener("click", (e) => player.right());
 
-const paint = _ => {
-  const mapa = stage1()
-  const pre = document.getElementById("game")
-  mapa[player.y][player.x] = player.c
-  pre.innerHTML = mapa.map(e => e.join("")).join("\n")
-}
+const paint = (_) => {
+  const mapa = stage1();
+  const pre = document.getElementById("game");
+  mapa[player.y][player.x] = player.c;
+  pre.innerHTML = mapa.map((e) => e.join("")).join("\n");
+};
 
-paint()
+paint();
